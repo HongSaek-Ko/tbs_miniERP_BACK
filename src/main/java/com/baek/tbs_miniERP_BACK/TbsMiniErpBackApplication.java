c10d7e34efca427100b7802b83a2cb0e094f64d0
HongSaek-ko <bh1152@naver.com>
2025-12-10 12:55:35 +0900

    Initial commit

---
+++ src/main/java/com/baek/tbs_miniERP_BACK/TbsMiniErpBackApplication.java
@@ -0,0 +1,13 @@
+package com.baek.tbs_miniERP_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbsMiniErpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbsMiniErpBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baek/tbs_miniERP_BACK/TbsMiniErpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baek.tbs_miniERP_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbsMiniErpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
